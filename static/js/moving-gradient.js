0d17bb9e98ad2f35ae0052cbb623c3c163075ded
Nehemia Telusma <telusma.nehemia@gmail.com>
2023-02-27 23:08:38 +0100

    feat: gradient effect

---
+++ static/js/moving-gradient.js
@@ -0,0 +1,13 @@
+const colored_text = document.getElementById("colored-name");
+
+colored_text.addEventListener("mousemove", (e)=>{
+  moveGradient(e.clientX, e.clientY);
+})
+
+function moveGradient(posX, posY) {
+  let = grandientX = window.getComputedStyle(colored_text).getPropertyValue('background-position-x');
+  let = grandientY = window.getComputedStyle(colored_text).getPropertyValue('background-position-Y');
+
+  colored_text.style.backgroundPositionX = ((parseInt(grandientX) + (posX/0.07)) / 100).toString()+"%";
+  colored_text.style.backgroundPositionY = ((parseInt(grandientY) + (posY/0.07)) / 100).toString()+"%";
+}
